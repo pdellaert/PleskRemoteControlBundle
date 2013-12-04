58a317ca4ebbb6d452ed568f2334a3c0f6d946f2
Philippe Dellaert <philippe@dellaert.org>
2013-12-04 16:22:38 +0100

    First plesk api test
    This is surely not final, you do not want to pass these parameters as
    get parameters!

---
+++ Controller/SubscriptionController.php
@@ -0,0 +1,66 @@
+<?php
+namespace Dellaert\PleskRemoteControl\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class Subscription extends Controller {
+    public function createAction($hostname,$ip,$ftplogin,$ftppass){
+        $url = $this->container->getParameter('dellaert_plesk_remote_control.baseurl');
+
+        $headers = array(
+            ‘HTTP_AUTH_LOGIN: ’.$this->container->getParameter('dellaert_plesk_remote_control.apiuser'),
+            ‘HTTP_AUTH_PASSWD: ’.$this->container->getParameter('dellaert_plesk_remote_control.apiuser'),
+            ‘Content-Type: text/xml’
+        );
+
+        $curl = curl_init();
+        // do not check the name of SSL certificate of the remote server 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        // do not check up the remote server certificate
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        // pass in the header elements
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        // pass in the url of the target server
+        curl_setopt($ch, CURLOPT_URL, $url);
+        // tell CURL to return the result rather than to load it to the browser
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $packet = '<?xml version="1.0" encoding="UTF-8"?>
+            <packet version="1.6.5.0">
+                <webspace>
+                    <add>
+                        <gen_setup>
+                            <name>'.$hostname.'</name>
+                            <htype>vrt_hst</htype>
+                            <ip_address>'.$ip.'</ip_address>
+                            <status>0</status>
+                        </gen_setup>
+                        <hosting>
+                            <vrt_hst>
+                                <property>
+                                    <name>ftp_login</name>
+                                    <value>'.$ftplogin.'</value>
+                                </property>
+                                <property>
+                                    <name>ftp_password</name>
+                                    <value>'.$ftppass.'</value>
+                                </property>
+                                <ip_address>'.$ip.'</ip_address>
+                            </vrt_hst>
+                        </hosting>
+                    </add>
+                </webspace>
+            </packet>';
+
+        // perform the CURL request and return the result 
+        $data = curl_exec($ch); 
+         
+        // close the CURL session
+        curl_close($ch); 
+
+        $response = new Response(json_encode($data));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+}
